3c645262ce9ad38894aca79e7a541111e8904543
DaveVodrazka <dave.vodrazka@gmail.com>
2024-11-14 12:50:56 +0100

    Squashed 'ui-kit/' changes from ec7c9cb..22f91c2
    22f91c2 fix: add dvh support
    
    git-subtree-dir: ui-kit
    git-subtree-split: 22f91c23a4eb759c4215d2bcbc50aed1932702f2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const plugin = require("tailwindcss/plugin");
+
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
@@ -30,5 +32,29 @@ module.exports = {
       },
     },
   },
-  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    plugin(function ({ addUtilities }) {
+      const fallbackHeightUtilities = {
+        "@supports not (height: 100dvh)": {
+          ".h-dvh": { height: "100vh" },
+          ".min-h-dvh": { "min-height": "100vh" },
+          ".max-h-dvh": { "max-height": "100vh" },
+        },
+        "@supports not (height: 100lvh)": {
+          ".h-lvh": { height: "100vh" },
+          ".min-h-lvh": { "min-height": "100vh" },
+          ".max-h-lvh": { "max-height": "100vh" },
+        },
+        "@supports not (height: 100svh)": {
+          ".h-svh": { height: "100vh" },
+          ".min-h-svh": { "min-height": "100vh" },
+          ".max-h-svh": { "max-height": "100vh" },
+        },
+      };
+
+      addUtilities(fallbackHeightUtilities, ["responsive"]);
+    }),
+  ],
 };
